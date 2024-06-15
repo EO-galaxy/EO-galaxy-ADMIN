652770814a8e83b20cbde658188aa218d0d04b9e
anonymousrecords <a.Records2022@gmail.com>
2024-06-15 23:01:47 +0900

    chore : Loader, Error 라우터 추가

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,20 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Home } from "../Home";
+import { Dashboard } from "../Dashboard";
 import { SignIn } from "../SignIn";
 import Layout from "../Layout/Layout";
+import Loader from "../Loader";
+import Error from "../Error";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    loader: () => <Loader />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
         element: <SignIn />,
       },
       {
-        path: "/home",
-        element: <Home />,
+        path: "/dashboard",
+        element: <Dashboard />,
       },
     ],
   },
