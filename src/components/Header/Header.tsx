9bea9f0a781c1f8fa1345c3f6b1dfbd9c3aa3c36
anonymousrecords <a.Records2022@gmail.com>
2024-06-15 23:00:07 +0900

    feat : Header 컴포넌트 구현

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+function Header() {
+  return (
+    <div css={HeaderStyles} >
+      <img
+        src="/logo.png"
+        alt="logo"
+        style={{ width: "28px", height: "28px" }}
+      />
+    </div>
+  );
+}
+
+export default Header;
+
+const HeaderStyles = css`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding-left: 130px;
+  height: 66.2px;
+  width: 100vw;
+  background-color: var(--bg-secondary);
+
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  @media (max-width: 768px) {
+    padding-left: 15px;
+  }
+`;
